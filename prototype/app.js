7a22bb89dac6afb0f03fc1cdc2c3a1fd93b6ef18
Mariana Gomes <marianagomes@Marianas-MacBook-Pro-2.local>
2020-11-01 21:43:02 -0500

    prototype code

---
+++ prototype/app.js
@@ -0,0 +1,43 @@
+// Everything that is in this file comes with node project when creating one
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+// This links the routes/index file (where all of our functionality for post/get lives) indexRouter variable, which is set up to the main path below.
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Wires indexRouter to the main path – localhost:3000/
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.showForm = showForm;
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
